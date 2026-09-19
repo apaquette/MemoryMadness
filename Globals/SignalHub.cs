c2030c07cc0f93749f5035c928a7085df3ec6c82
Alex Paquette <alexandre.d.paquette@gmail.com>
2026-09-18 21:46:10 -0300

    refactor signalhub

--- Globals/SignalHub.cs
+++ Globals/SignalHub.cs
@@ -9,27 +9,10 @@ public partial class SignalHub : Node
 	[Signal] public delegate void OnLevelExitEventHandler();
 	[Signal] public delegate void OnMoveMadeEventHandler();
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		Instance = this;
-	}
+	public override void _Ready() => Instance = this;
 
-	public static void EmitOnTileSelected(MemoryTile memoryTile)
-	{
-		Instance.EmitSignal(SignalName.OnTileSelected, memoryTile);
-	}
-
-	public static void EmitOnLevelSelected(LevelSetting levelSetting)
-	{
-		Instance.EmitSignal(SignalName.OnLevelSelected, levelSetting);
-	}
-	public static void EmitOnLevelExit()
-	{
-		Instance.EmitSignal(SignalName.OnLevelExit);
-	}
-
-	public static void EmitOnMoveMade(int moves, int pairs)
-	{
-		Instance.EmitSignal(SignalName.OnMoveMade, moves, pairs);
-	}
+	public static void EmitOnTileSelected(MemoryTile memoryTile) => Instance.EmitSignal(SignalName.OnTileSelected, memoryTile);
+	public static void EmitOnLevelSelected(LevelSetting levelSetting) => Instance.EmitSignal(SignalName.OnLevelSelected, levelSetting);
+	public static void EmitOnLevelExit() => Instance.EmitSignal(SignalName.OnLevelExit);
+	public static void EmitOnMoveMade(int moves, int pairs) => Instance.EmitSignal(SignalName.OnMoveMade, moves, pairs);
 }
